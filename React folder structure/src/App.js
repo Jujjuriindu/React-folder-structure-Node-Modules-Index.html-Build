1a1e735d99f7113111adbd0dd8f27cc4cf8c7f5f
Jujjuriindu <140819241+Jujjuriindu@users.noreply.github.com>
2024-11-17 20:13:07 +0530

    Add files via upload

---
+++ React folder structure/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Cakes are very tasty and delicious</h1>
+      <img src="https://wildwildwhisk.com/wp-content/uploads/2020/03/Mini-Chocolate-Cake-1.jpg"></img>
+    </div>
+  );
+}
+
+export default App;
